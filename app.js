dd0dd3c71461aef02d92e560c5e56759883a035c
naracodes <naralee0124@gmail.com>
2020-10-21 00:55:51 -0400

    add text to render on scroll in segments

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const text2 = intro.querySelector('h2');
 const controller = new ScrollMagic.Controller();
 
 
-
 const flightPath = {
     //curviness of 0 is rigid motion
     curviness: 1.25,
@@ -43,26 +42,80 @@ tween.add(
 
 let scene = new ScrollMagic.Scene({
     triggerElement: intro,
-    duration: 20000,
+    duration: 8000,
     triggerHook: 0
 })
     .setTween(tween)
     .addIndicators()
     .setPin('.intro')
     .addTo(controller)
+    
+// function fade() {
+//   var target = $(this).find(".box");
+//   var tl = new TimelineMax();
+// }
 
-const textAnim = TweenMax.fromTo(text, 3, { opacity: 0}, {opacity: 1});
-const textAnim2 = TweenMax.fromTo(text2, 3, { opacity: 0}, {opacity: 1});
+// $(".section").each(function (i) {
+//   var target = $(this).find(".box");
+//   var tl = new TimelineMax();
 
-let scene2 = new ScrollMagic.Scene({
-    duration: 10000,
-    triggerElement: intro,
-    triggerHook: 0,
+//   tl.from(target, 1, { opacity: 0 });
+//   tl.to(target, 1, { opacity: 0 }, 0.75);
+
+//   new ScrollMagic.Scene({
+//     triggerElement: intro,
+//     triggerHook: 0,
+//     duration: '392%'
+//   })
+//     // .setPin(this)
+//     // .setTween(tween)
+//     .setTween(tl)
+//     .addIndicators()
+//     .addTo(controller);
+// });
+
+let target1 = $('.section1').find('.box');
+let tl1 = new TimelineMax();
+
+tl1.from(target1, 1, { opacity: 0 });
+tl1.to(target1, 1, { opacity: 0 }, 0.75);
+
+new ScrollMagic.Scene({
+triggerElement: intro,
+triggerHook: 0,
+duration: '392%'
 })
-
-.setTween(textAnim)
+.setTween(tl1)
 .addIndicators()
-.addTo(controller)
+.addTo(controller);
+
+let target2 = $('.section2').find('.box');
+let tl2 = new TimelineMax();
+
+tl2.from(target2, 1, { opacity: 0 });
+tl2.to(target2, 1, { opacity: 0 }, 0.75);
+
+new ScrollMagic.Scene({
+triggerElement: intro,
+triggerHook: 0,
+duration: '392%'
+})
+.setTween(tl2)
+.addIndicators()
+.addTo(controller);
+
+// const textAnim = TweenMax.fromTo(text, 3, { opacity: 0}, {opacity: 1});
+// const textAnim2 = TweenMax.fromTo(text2, 3, { opacity: 0}, {opacity: 1});
+
+// let scene2 = new ScrollMagic.Scene({
+//     duration: 6800,
+//     triggerElement: intro,
+//     triggerHook: 0,
+// })
+
+// .setTween(textAnim)
+// .addIndicators()
+// .addTo(controller)
 
 // let scrollpos = 0;
 
